e74c463bfe003250583d65359c710bbee856a8d9
Enieber Cunha <7907068+enieber@users.noreply.github.com>
2024-04-13 03:47:12 -0300

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -22,33 +22,27 @@ echo "Lista de pastas gerada em $saida"
 
 }
 
-get_all()
+clone()
 {
-  echo -e "deps of oca\n"
-  export VERSION=13.0
-  git clone -b ${VERSION} https://github.com/oca/l10n-brazil                                                       
-  git clone -b ${VERSION}  https://github.com/oca/account-financial-reporting                                       
-  git clone -b ${VERSION}  https://github.com/oca/account-payment                                                    
-  git clone -b ${VERSION}  https://github.com/oca/bank-payment                                                       
-  git clone -b ${VERSION}  https://github.com/oca/account-invoicing                                                  
-  git clone -b ${VERSION}  https://github.com/oca/commission                                                         
-  git clone -b ${VERSION}  https://github.com/oca/web                                                                
-  git clone -b ${VERSION}  https://github.com/oca/mis-builder                                                        
-  git clone -b ${VERSION}  https://github.com/oca/server-ux                                                          
-  git clone -b ${VERSION}  https://github.com/oca/reporting-engine                                                   
-  git clone -b ${VERSION}  https://github.com/oca/website-themes 
-  git clone -b ${VERSION}  https://github.com/oca/field-service
-  git clone -b ${VERSION}  https://github.com/oca/geospatial
-  git clone -b ${VERSION}  https://github.com/oca/partner-contact
-  git clone -b ${VERSION}  https://github.com/oca/stock-logistics-warehouse
-  git clone -b ${VERSION}  https://github.com/oca/sale-workflow
+echo -e "deps of oca\n"
+export VERSION=$1
+export FILE=$2
+export REPLACE=$3
+
+for item in $(cat $FILE); do
+   if [ -d "$item" ]; then
+        project=$(echo "$item" | sed "s|"$REPLACE"||")
+        echo "$project"
+        git clone -b ${VERSION} https://github.com/oca/${project}
+    fi
+done
 }
 
 
 help()
 {
   echo -e "\n helper to deps of oca odoo \n"
-  echo "get_all - clone all dependencies from branch with version"
+  echo -e "clone - clone all dependencies from branch with version: \n $0 clone VERSION FILE_LIST REPLACE \n exeample: clone 12.0 a.txt /opt/oca\n"
   echo "get_deps_folder - list folde and pass intem to file, sample: $0 get . a.txt $"PWD""
 }
 
