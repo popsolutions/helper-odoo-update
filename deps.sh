76ad2b712cd9c802a218af2ce4972818537bf3a3
Enieber Cunha <7907068+enieber@users.noreply.github.com>
2024-04-13 02:26:22 -0300

    Create deps.sh

---
+++ deps.sh
@@ -0,0 +1,18 @@
+echo -e "deps of oca\n"
+export VERSION=13.0
+git clone -b ${VERSION} https://github.com/oca/l10n-brazil                                                       
+git clone -b ${VERSION}  https://github.com/oca/account-financial-reporting                                       
+git clone -b ${VERSION}  https://github.com/oca/account-payment                                                    
+git clone -b ${VERSION}  https://github.com/oca/bank-payment                                                       
+git clone -b ${VERSION}  https://github.com/oca/account-invoicing                                                  
+git clone -b ${VERSION}  https://github.com/oca/commission                                                         
+git clone -b ${VERSION}  https://github.com/oca/web                                                                
+git clone -b ${VERSION}  https://github.com/oca/mis-builder                                                        
+git clone -b ${VERSION}  https://github.com/oca/server-ux                                                          
+git clone -b ${VERSION}  https://github.com/oca/reporting-engine                                                   
+git clone -b ${VERSION}  https://github.com/oca/website-themes 
+git clone -b ${VERSION}  https://github.com/oca/field-service
+git clone -b ${VERSION}  https://github.com/oca/geospatial
+git clone -b ${VERSION}  https://github.com/oca/partner-contact
+git clone -b ${VERSION}  https://github.com/oca/stock-logistics-warehouse
+git clone -b ${VERSION}  https://github.com/oca/sale-workflow
